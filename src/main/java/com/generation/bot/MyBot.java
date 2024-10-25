9658420153df54bb35da57cd62f57d6bd6a0a1eb
Anvar Ibragimov <technoprogrammer809@gmail.com>
2024-10-25 17:17:19 +0500

    Added

--- src/main/java/com/generation/bot/MyBot.java
+++ src/main/java/com/generation/bot/MyBot.java
@@ -10,28 +10,37 @@ public class MyBot extends TelegramLongPollingBot {
         super(botToken);
     }
 
+    String firstName;
+    String lastName;
+    String phoneNumber;
+    String state = "START";
+
     @Override
     public void onUpdateReceived(Update update) {
-        String pupilId = String.valueOf(0L);
-        String adminId = String.valueOf(0L);
-        String userId = String.valueOf(update.getMessage().getChatId());
-        String sendText = update.getMessage().getText();
-
-        System.out.println(update.getMessage().getChatId() + " | " + update.getMessage().getText());
+        String chatId = update.getMessage().getChatId().toString();
+        String text = update.getMessage().getText();
 
         SendMessage sendMessage = new SendMessage();
-        if (sendText.equals("/start")) {
-            sendMessage.setChatId(userId);
-            sendMessage.setText("Assalom alaykum botimizga x .... admin uchun xabarizni yuboring!");
-        } else {
-            sendMessage.setText(sendText);
-            if (userId.equals(pupilId)) {
-                sendMessage.setChatId(adminId);
-            } else if (userId.equals(adminId)) {
-                sendMessage.setChatId(pupilId);
+        sendMessage.setChatId(chatId);
+
+        if(text.equals("/start")){
+            sendMessage.setText("Assalom alaykum ismizni kiriting!");
+            state = "FIRSTNAME";
+        }else {
+            if(state.equals("FIRSTNAME")){
+                firstName = text;
+                sendMessage.setText("Familiyezni kiriting!");
+                state = "LASTNAME";
+            }else if(state.equals("LASTNAME")){
+                lastName = text;
+                sendMessage.setText("Raqamizni kiriting!");
+                state = "REGISTERED";
+            }else if(state.equals("REGISTERED")){
+                phoneNumber = text;
+                sendMessage.setText("Siz muvaffaqiyatli ro'yxatdan o'tdiz iltimos admin javobini kuting!");
+                System.out.println("Foydalanuvchi: " + firstName + " | " + lastName + " | " + phoneNumber);
             }
         }
-
 
         try {
             execute(sendMessage);
